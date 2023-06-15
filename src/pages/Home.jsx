0a7d04a28826474dca73954007c9c9448b7fcd6c
AnirCoredevs <abu_bakkar@gmail.com>
2023-06-15 15:04:36 +0600

    assests added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Home() {
   return (
-    <h1 className="md:text-h1-md text-h1">Home page</h1>
+    <h1 className="md:text-h1-xl text-h1">Home page</h1>
   );
 }
 
