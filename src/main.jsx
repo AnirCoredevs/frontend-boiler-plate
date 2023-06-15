ef6e1773cdb3b9725479a3f00c7bacab36f1e4f5
AnirCoredevs <abu_bakkar@gmail.com>
2023-06-15 15:48:00 +0600

    service worker file addded

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
+import { SnackbarProvider } from 'notistack';
 import App from './App';
 import './index.css';
 import store from './store/store';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <SnackbarProvider maxSnack={3}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </SnackbarProvider>
   </React.StrictMode>,
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
